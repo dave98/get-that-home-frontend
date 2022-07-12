36615f147054ce6f5a16f93dc857defdccac8eee
Revocod3 <kevinlopez2693@gmail.com>
2022-07-12 02:49:42 -0400

    refactor typography

--- src/style/typography.js
+++ src/style/typography.js
@@ -1,87 +1,86 @@
-
-export const fonts={
+export const fonts = {
   primary: `
     'Josefin Sans', sans-serif,"Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue"
   `,
 
-  secondary:`
+  secondary: `
     "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI",
     Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif
-  `
-}
+  `,
+};
 
 export const typography = {
-  head:{
-    xxl:`
+  head: {
+    xxl: `
       font-size: 96px;
       line-height: 128px;
-      font-weight: 300;
     `,
-    xl:`
+    xl: `
     font-size: 64px;
     line-height: 88px;
-    font-weight: 300;
     `,
-    l:`
+    l: `
       font-size: 48px;
       line-height: 64px;
-      font-weight: 400;
     `,
-    md:`
+    md: `
       font-size: 36px;
       line-height: 48px;
-      font-weight: 400;
     `,
-    sm:`
+    sm: `
       font-size: 24px;
       line-height: 32px;
-      font-weight: 400;
     `,
-    xs:`
+    xs: `
       font-size: 20px;
       line-height: 28px;
-      font-weight: 500;
-    `
+    `,
   },
-  subtitle:{
-    sm:`
+  body: {
+    sm: `
     font-size: 16px;
     line-height: 24px;
-    font-weight: 400;
     `,
-    xs:`
+    xs: `
     font-size: 14px;
     line-height: 20px;
-    font-weight: 500;
-    `
-  },
-  body:{
-    sm:`
-    font-size: 16px;
-    line-height: 24px;
-    font-weight: 400;
     `,
-    xs:`
-    font-size: 14px;
-    line-height: 20px;
-    font-weight: 400;
-    `
   },
-  others:{
-    button:`
+  others: {
+    button: `
     font-size: 14px;
     line-height: 24px;
-    font-weight: 500;
     `,
-    caption:`
+    caption: `
     font-size: 14px;
     line-height: 16px;
-    font-weight: 400;
     `,
-    overline:`
+    overline: `
     font-size: 10px;
     line-height: 16px;
+    `,
+  },
+  weight: {
+    300: `
+    font-weight: 300;
+    `,
+    400: `
     font-weight: 400;
     `,
-  }
-}
+    500: `
+    font-weight: 500;
+    `,
+    600: `
+    font-weight: 600;
+    `,
+    700: `
+    font-weight: 700;
+    `,
+    800: `
+    font-weight: 800;
+    `,
+    900: `
+    font-weight: 900;
+    `,
+  },
+};
