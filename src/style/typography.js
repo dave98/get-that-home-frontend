0f2c6f7bccfff56308fadf63bd8e1edf00f795e5
Enrique Vizarra <vizarramattay@gmail.com>
2022-07-11 19:43:59 -0500

    ADD: typography.js

---
+++ src/style/typography.js
@@ -0,0 +1,87 @@
+
+export const fonts={
+  primary: `
+    'Josefin Sans', sans-serif,"Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue"
+  `,
+
+  secondary:`
+    "Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif
+  `
+}
+
+export const typography = {
+  head:{
+    xxl:`
+      font-size: 96px;
+      line-height: 128px;
+      font-weight: 300;
+    `,
+    xl:`
+    font-size: 64px;
+    line-height: 88px;
+    font-weight: 300;
+    `,
+    l:`
+      font-size: 48px;
+      line-height: 64px;
+      font-weight: 400;
+    `,
+    md:`
+      font-size: 36px;
+      line-height: 48px;
+      font-weight: 400;
+    `,
+    sm:`
+      font-size: 24px;
+      line-height: 32px;
+      font-weight: 400;
+    `,
+    xs:`
+      font-size: 20px;
+      line-height: 28px;
+      font-weight: 500;
+    `
+  },
+  subtitle:{
+    sm:`
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+    `,
+    xs:`
+    font-size: 14px;
+    line-height: 20px;
+    font-weight: 500;
+    `
+  },
+  body:{
+    sm:`
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+    `,
+    xs:`
+    font-size: 14px;
+    line-height: 20px;
+    font-weight: 400;
+    `
+  },
+  others:{
+    button:`
+    font-size: 14px;
+    line-height: 24px;
+    font-weight: 500;
+    `,
+    caption:`
+    font-size: 14px;
+    line-height: 16px;
+    font-weight: 400;
+    `,
+    overline:`
+    font-size: 10px;
+    line-height: 16px;
+    font-weight: 400;
+    `,
+  }
+}
