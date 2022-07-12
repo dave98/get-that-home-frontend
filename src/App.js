dc018f315b0f5bc7f0c9d3dbb0e21ce0b0b552b6
Revocod3 <kevinlopez2693@gmail.com>
2022-07-12 16:50:52 -0400

    Add: structure of login with router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import styled from "@emotion/styled";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import LoginPage from "./pages/login-page";
 
 const MainWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   position: relative;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
 `;
 
 const App = () => {
@@ -13,6 +19,12 @@ const App = () => {
   return (
     <MainWrapper>
       <Navbar isAuth={isAuth} role={"seller"} />
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="join" element={<div>Join</div>} />
+        <Route path="properties" element={<div>Properties</div>} />
+      </Routes>
     </MainWrapper>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Global } from "@emotion/react";
 import { global, reset } from "./style/global";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <Global styles={global} />
     <Global styles={reset} />
-    <App />
+    <Router>
+      <App />
+    </Router>
   </>
 );
