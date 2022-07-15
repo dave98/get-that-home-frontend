5c1e9af44444fb12a8e8d33f881160de8fafcb7a
Revocod3 <kevinlopez2693@gmail.com>
2022-07-15 03:18:12 -0400

    finished landing

--- src/components/CallToAction/call-to-action.js
+++ src/components/CallToAction/call-to-action.js
@@ -1,15 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import { colors } from "../../style/colors";
 import Button from "../Button";
 import { Heading } from "../typography";
 import { CallToActionWrapper } from "./style";
 
 const CallToAction = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("join");
+  };
+
   return (
     <CallToActionWrapper>
       <Heading color={colors.grays.dark} weight={400}>
         Getting someone to rent your apartment has never been this easy
       </Heading>
-      <Button>CREATE AN ACCOUNT NOW</Button>
+      <Button onClick={handleClick}>CREATE AN ACCOUNT NOW</Button>
     </CallToActionWrapper>
   );
 };
