dec21a9e6d50e3226f8aefa094e76b86532b415b
Enrique Vizarra <vizarramattay@gmail.com>
2022-07-20 18:52:33 -0500

    ADD: Show-porperty-data

---
+++ src/components/ShowPropertyData/style.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { colors } from "../../style";
+
+
+
+export const ShowPropertyDataWapper = styled.div`
+  width: 830px;
+  height: 394px;
+  background-color: ${colors.grays.shallow};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+ 
+`;
+
+export const ShowAddressContainer = styled.div`
+  width: 766px;
+  height: 76px;
+  background-color: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center
+`
+export const ShowAddressContainer2 = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+export const ShowDetailsContainer = styled.div`
+  width: 766px;
+  height: 66px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+export const AboutPropertycontainer = styled.div`
+  width: 760px;
+  height: 204px;
+`
