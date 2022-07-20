ee0fdf6081c7bdbe37e01e8a764e7c68f1dc144d
Revocod3 <kevinlopez2693@gmail.com>
2022-07-20 16:56:23 -0400

    Add: PropertiesContext & implemented hero search button navigation

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -1,21 +1,13 @@
-import { useEffect, useState } from "react";
 import CallToAction from "../components/CallToAction";
 import BestPropertiesSection from "../components/DisplayBestProperties/best-properties-section";
 import Hero from "../components/Hero";
 import TeamComponent from "../components/TeamComponent";
-import { getProperties } from "../services/properties-service";
 
 const LandingPage = () => {
-  const [properties, setProperties] = useState([]);
-
-  useEffect(() => {
-    getProperties().then(setProperties).catch(console.error);
-  }, []);
-
   return (
     <>
       <Hero />
-      <BestPropertiesSection properties={properties} />
+      <BestPropertiesSection />
       <CallToAction />
       <TeamComponent />
     </>
