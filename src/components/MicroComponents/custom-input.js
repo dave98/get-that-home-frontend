b32a17a7ab5601d0f2599ed7df9eaaf8d0b4c7db
Revocod3 <kevinlopez2693@gmail.com>
2022-07-22 03:00:27 -0400

    add: filters

--- src/components/MicroComponents/custom-input.js
+++ src/components/MicroComponents/custom-input.js
@@ -1,30 +1,51 @@
-import { CustomErrorMessage, CustomInputBox, CustomInputContainer, CustomInputLabel, ErrorContainer, SurrounderInputContainer } from "./styles";
+import {
+  CustomErrorMessage,
+  CustomInputBox,
+  CustomInputContainer,
+  CustomInputLabel,
+  ErrorContainer,
+  SurrounderInputContainer,
+} from "./styles";
 
-export const CustomInput = ({ type="text", name, label="", value, onChange, placeholder, icon, error, errorDirection="column", isTouched, isValid, onBlur, ...props} ) => {
-  const globalName = name.toLocaleLowerCase().replace(/ /g, "_");
+export const CustomInput = ({
+  type = "text",
+  name,
+  label = "",
+  value,
+  onChange,
+  placeholder,
+  icon,
+  error,
+  errorDirection = "column",
+  isTouched,
+  isValid,
+  onBlur,
+  ...props
+}) => {
+  const globalName = label.toLocaleLowerCase().replace(/ /g, "_");
 
   return (
     <ErrorContainer errorDirection={errorDirection}>
       <SurrounderInputContainer type={type} errorDirection={errorDirection}>
-          {label && <CustomInputLabel>{label}</CustomInputLabel>}
-          <CustomInputContainer>
-            {icon && icon}
-            <CustomInputBox
-              type={type}
-              id={globalName}
-              name={name || globalName}
-              value={value}
-              onBlur={onBlur}
-              onChange={onChange}
-              placeholder={placeholder}
-              isValid={isValid}
-              accept="image/png, image/jpeg"
-              multiple
-              {...props}
-            />
-          </CustomInputContainer>
+        {label && <CustomInputLabel>{label}</CustomInputLabel>}
+        <CustomInputContainer>
+          {icon && icon}
+          <CustomInputBox
+            type={type}
+            id={globalName}
+            name={name || globalName}
+            value={value}
+            onBlur={onBlur}
+            onChange={onChange}
+            placeholder={placeholder}
+            isValid={isValid}
+            accept="image/png, image/jpeg"
+            multiple
+            {...props}
+          />
+        </CustomInputContainer>
       </SurrounderInputContainer>
-      {error && isTouched && <CustomErrorMessage>{error}</CustomErrorMessage> } 
+      {error && isTouched && <CustomErrorMessage>{error}</CustomErrorMessage>}
     </ErrorContainer>
   );
 };
